9692c6d5baef5d376f276a7315a11fc333a7c608
Thierry Chantier <titimoby@gmail.com>
2024-09-27 14:54:25 +0200

    add basic test for vscode snippet generation

---
+++ convert-to-vscode_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestGenerateJsonSnippets(t *testing.T) {
+	yamlFilePath := "samples/python01.yml"
+	yamlData, err := readYamlFile(yamlFilePath)
+	if err != nil {
+		t.Errorf("failed to read YAML file: %v", err)
+		return
+	}
+
+	jsonSnippets, err := generateJsonSnippets(yamlData)
+	if err != nil {
+		t.Errorf("failed to generate JSON snippets: %v", err)
+		return
+	}
+
+	codeSnippetsFilePath := "samples/python01.code-snippets"
+	expectedJsonSnippets, err := os.ReadFile(codeSnippetsFilePath)
+	if err != nil {
+		t.Errorf("failed to read code snippets file: %v", err)
+		return
+	}
+
+	computedSnippet := string(jsonSnippets)
+	expectedSnippet := string(expectedJsonSnippets)
+	if computedSnippet != expectedSnippet {
+		t.Errorf("generated JSON snippets do not match expected output: \n%v\nvs\n%v", computedSnippet, expectedSnippet)
+	}
+}
